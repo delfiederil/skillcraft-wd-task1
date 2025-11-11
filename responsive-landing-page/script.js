69eb2869d18c594651d8f57b4c80bf94525a66ee
Delfie Deril <delfiederil30@gmail.com>
2025-11-11 15:39:39 +0530

    Create script.js

---
+++ responsive-landing-page/script.js
@@ -0,0 +1,4 @@
+document.getElementById('getStartedBtn').addEventListener('click',function(e){
+  e.preventDefault();
+  document.querySelector('#contact').scrollIntoView({behaviour:'smooth'});
+});
